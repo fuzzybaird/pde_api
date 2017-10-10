ea5e36225a6f0cbdabdbe801197412bffb932774
Eli Baird <eli.baird@chghealthcare.com>
2017-10-10 16:13:59 -0600

    Initial API commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(App\Assignment::class, function (Faker $faker) {
+    return [
+        'description' => $faker->paragraph,
+        'hospital' => $faker->word,
+        'start_date' => $faker->dateTime(),
+        'end_date' => $faker->dateTime(),
+    ];
+});
